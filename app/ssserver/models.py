4390787ecf2c913398d86f4695242ab9c893b27c
gfwbrker <gfwbrker@gmail.com>
2018-03-09 21:54:04 +0800

    update method

--- app/ssserver/models.py
+++ app/ssserver/models.py
@@ -14,11 +14,24 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 from shadowsocks.tools import get_short_random_string, traffic_format
 
 METHOD_CHOICES = (
+    ('aes-128-gcm', 'aes-128-gcm'),
+    ('aes-192-gcm', 'aes-192-gcm'),
+    ('aes-256-gcm', 'aes-256-gcm'),
+    ('rc4-md5', 'rc4-md5'),
+    ('aes-128-cfb', 'aes-128-cfb'),
+    ('aes-192-cfb', 'aes-192-cfb'),
     ('aes-256-cfb', 'aes-256-cfb'),
     ('aes-128-ctr', 'aes-128-ctr'),
-    ('rc4-md5', 'rc4-md5'),
+    ('aes-192-ctr', 'aes-192-ctr'),
+    ('aes-256-ctr', 'aes-256-ctr'),
+    ('bf-cfb', 'bf-cfb'),
+    ('camellia-128-cfb', 'camellia-128-cfb'),
+    ('camellia-192-cfb', 'camellia-192-cfb'),
+    ('camellia-256-cfb', 'camellia-256-cfb'),
+    ('chacha20-ietf-poly1305', 'chacha20-ietf-poly1305'),
     ('salsa20', 'salsa20'),
     ('chacha20', 'chacha20'),
+    ('chacha20-ietf', 'chacha20-ietf'),
     ('none', 'none'),
 )
 
